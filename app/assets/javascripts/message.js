aca147caafe0b62e059670235bdb2fff4b0ac9b1
ken111626 <setsuko.t-09.26-gonzales@docomo.ne.jp>
2020-03-27 13:12:13 +0900

    chat-space 修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,8 @@ $(function(){
   function buildHTML(message){
     if (message.image) {
       var html =
-        `<div class="main-chat__message-list__message">
+        `<div class="message" data-message-id="${message.id}">
+        <div class="main-chat__message-list__message">
           <div class="main-chat__message-list__message__upper-info">
             <div class="main-chat__message-list__message__upper-info__talker">
               ${message.user_name}
@@ -17,11 +18,13 @@ $(function(){
             </p>
             <img src=${message.image}>
           </div>
+        </div>
         </div>`
       return html;
     }else{
       var html =
-        `<div class="main-chat__message-list__message">
+        `<div class="message" data-message-id="${message.id}">
+        <div class="main-chat__message-list__message">
           <div class="main-chat__message-list__message__upper-info">
             <div class="main-chat__message-list__message__upper-info__talker">
               ${message.user_name}
@@ -35,6 +38,7 @@ $(function(){
               ${message.content}
             </p>
           </div>
+        </div>
         </div>`
       return html;
     };
@@ -57,10 +61,35 @@ $(function(){
       $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
       $('form')[0].reset(); 
       $('.main-chat__message-form__new-message__submit-btn').prop('disabled', false);
+      return false;
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
     })
-    return false;
   })
+  var reloadMessages = function() {
+    var last_message_id = $('.messages:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.main-chat__message-list').append(insertHTML);
+      $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+     }
+    })
+    .fail(function() {
+      alert('error');
+    });
+   };
+   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+   }
 });
